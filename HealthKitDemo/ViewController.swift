80dff3bda934dccf511ce5ba7c9c315fd4c0378a
Benoit PASQUIER <b.pasquier69@gmail.com>
2016-06-14 21:07:28 +0100

    Initial Commit

---
+++ HealthKitDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HealthKitDemo
+//
+//  Created by Benoit PASQUIER on 14/06/2016.
+//  Copyright © 2016 Benoit PASQUIER. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
